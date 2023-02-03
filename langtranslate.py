9a2867e47bd95b69f6e9e16faaba2a172b3f20b5
jinoprinx <jino4rex@gmail.com>
2023-02-03 22:46:47 +0100

    reverted def block

--- langtranslate.py
+++ langtranslate.py
@@ -1,17 +1,19 @@
 from translate import Translator
 
-
-#lang_1= input("Enter the Language you wish to translate from __:")
-#lang_2= input("Enter the Language you wish to translate to __:")
-#text= input("Enter the Text you wish to translate  __:")
-#translator= Translator(lang_1, lang_2)
-#translated_text= translator.translate(text)
-#print(translated_text)
-
-#On a second thought
-def transLate(lang_1, lang_2, text):
+def langTranslate(text):
+	lang_1= input("Enter the Language you wish to translate from __:")
+	lang_2= input("Enter the Language you wish to translate to __:")
+	text= input("Enter the Text you wish to translate  __:")
 	translator= Translator(lang_1, lang_2)
 	translated_text= translator.translate(text)
 	print(translated_text)
+langTranslate("hello how are you")
 
-transLate("english", "spanish", "hello how are you")
+
+#On a second thought
+#def transLate(lang_1, lang_2, text):
+#	translator= Translator(lang_1, lang_2)
+#	translated_text= translator.translate(text)
+#	print(translated_text)
+
+#transLate("english", "spanish", "hello how are you")
