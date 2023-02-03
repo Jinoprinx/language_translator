e59b7be85a8e83430b4ae40cff8fec531906b427
jinoprinx <jino4rex@gmail.com>
2023-02-03 22:56:08 +0100

    added inputfunctio

--- langtranslate.py
+++ langtranslate.py
@@ -1,19 +1,22 @@
 from translate import Translator
 
-def langTranslate(text):
-	lang_1= input("Enter the Language you wish to translate from __:")
-	lang_2= input("Enter the Language you wish to translate to __:")
-	text= input("Enter the Text you wish to translate  __:")
-	translator= Translator(lang_1, lang_2)
-	translated_text= translator.translate(text)
-	print(translated_text)
-langTranslate("hello how are you")
+# def langTranslate(text):
+# 	lang_1= input("Enter the Language you wish to translate from __:")
+# 	lang_2= input("Enter the Language you wish to translate to __:")
+# 	text= input("Enter the Text you wish to translate  __:")
+# 	translator= Translator(lang_1, lang_2)
+# 	translated_text= translator.translate(text)
+# 	print(translated_text)
+# langTranslate("hello how are you")
 
 
 #On a second thought
-#def transLate(lang_1, lang_2, text):
-#	translator= Translator(lang_1, lang_2)
-#	translated_text= translator.translate(text)
-#	print(translated_text)
+def transLate():
+	lang_1= input("enter Language you wish to translate from__:")
+	lang_2= input("enter Language you wish to translate to__:")
+	text= input("enter the text you wish to translate __:")
+	translator= Translator(lang_1, lang_2)
+	translated_text= translator.translate(text)
+	print(translated_text)
 
-#transLate("english", "spanish", "hello how are you")
+transLate()
