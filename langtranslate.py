290334a56a990f494bf3130894244c988eb444cf
jinoprinx <jino4rex@gmail.com>
2023-02-03 07:03:52 +0100

    re-arrange translation script

--- langtranslate.py
+++ langtranslate.py
@@ -1,11 +1,17 @@
 from translate import Translator
 
-def text_translate():
-	lang_1= input("Enter the Language you wish to translate from __:")
-	lang_2= input("Enter the Language you wish to translate to __:")
-	text= input("Enter the Text you wish to translate  __:")
-	translator= Translator(lang_1, lang_2)
-	translate_text= translator.translate(text)
-	print(translate_text)
 
-text_translate()
+#lang_1= input("Enter the Language you wish to translate from __:")
+#lang_2= input("Enter the Language you wish to translate to __:")
+#text= input("Enter the Text you wish to translate  __:")
+#translator= Translator(lang_1, lang_2)
+#translated_text= translator.translate(text)
+#print(translated_text)
+
+#On a second thought
+def transLate(lang_1, lang_2, text):
+	translator= Translator(lang_1, lang_2)
+	translated_text= translator.translate(text)
+	print(translated_text)
+
+transLate("english", "spanish", "hello how are you")
