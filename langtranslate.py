5bc25aae3efca7e519e99690552d4bf18dce0387
jinoprinx <jino4rex@gmail.com>
2023-03-14 14:33:46 +0100

    wrapped function inside gradio interface

--- langtranslate.py
+++ langtranslate.py
@@ -1,21 +1,10 @@
+import gradio as gr
 from translate import Translator
 
-# def langTranslate(text):
-# 	lang_1= input("Enter the Language you wish to translate from __:")
-# 	lang_2= input("Enter the Language you wish to translate to __:")
-# 	text= input("Enter the Text you wish to translate  __:")
-# 	translator= Translator(lang_1, lang_2)
-# 	translated_text= translator.translate(text)
-# 	print(translated_text)
-# langTranslate("hello how are you")
+def transLate(English_text, Choose_language):
+	translator= Translator(from_lang="english", to_lang=Choose_language)
+	return translator.translate(English_text)
 
+lang= gr.Interface(fn=transLate, inputs=["text", "text"], outputs="text")
 
-#On a second thought
-def transLate():
-	lang_1= input("enter Language you wish to translate from__:")
-	lang_2= input("enter Language you wish to translate to__:")
-	text= input("enter the text you wish to translate __:")
-	translator= Translator(from_lang=lang_1, to_lang=lang_2)
-	print(translator.translate(text))
-
-transLate()
+lang.launch()
